4e756bd4128a267b8b94a94d4d1feef18abfd212
tienanh0903k <tienanh2003k@gmail.com>
2025-07-18 23:29:18 +0700

    add term

---
+++ client/src/app/pages/admin-system/periods/periods.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-periods',
+  imports: [],
+  templateUrl: './periods.component.html',
+  styleUrl: './periods.component.scss'
+})
+export class PeriodsComponent {
+
+}
